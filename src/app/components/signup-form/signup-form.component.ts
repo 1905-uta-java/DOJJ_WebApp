98e40b0924b655ff734b4d7d9427a09ad18478ba
MartinD4574 <dcm9595@gmail.com>
2019-07-07 10:09:03 -0500

    Formated and Commented Errors
    Errors In signup-form
    Errors with leaflet in theater.ts
    Formated Change Password
    Formated Profile
    Formated SignUp

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -3,24 +3,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsernameValidators } from './username.validators';
 
 @Component({
-  selector: 'signup-form',
+  selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
   form = new FormGroup({
     account: new FormGroup({
-      'username': new FormControl('', [
+      username: new FormControl('', [
         Validators.required,
         Validators.minLength(3),
         UsernameValidators.cannotContainSpace
-      ], 
+      ],
       UsernameValidators.shouldBeUnique),
-      'password': new FormControl('', Validators.required)
+      password: new FormControl('', Validators.required)
     })
   });
 
-  login(){
+  login() {
     this.form.setErrors({
       invalidLogin: true
     });
