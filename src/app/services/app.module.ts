03baf0bb11a4a0c01833e412ea79450670f81f2b
omar-nav <omar.navarro@g.austincc.edu>
2019-07-09 11:20:26 -0500

    add url for post request of new users

--- src/app/services/app.module.ts
+++ src/app/services/app.module.ts
@@ -5,27 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
-import { MovieComponent } from './components/movie/movie.component';
-import { LoginDropdownComponent } from './components/login-dropdown/login-dropdown.component';
-import { AccountDropdownComponent } from './components/account-dropdown/account-dropdown.component';
-import { RecentsComponent } from './components/recents/recents.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { SearchComponent } from '../components/search/search.component';
+import { MovieComponent } from '../components/movie/movie.component';
+import { LoginDropdownComponent } from '../components/login-dropdown/login-dropdown.component';
+import { AccountDropdownComponent } from '../components/account-dropdown/account-dropdown.component';
+import { RecentsComponent } from '../components/recents/recents.component';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateReviewComponent } from './components/create-review/create-review.component';
-import { RatingComponent } from './components/rating/rating.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { DisplayReviewsComponent } from './components/display-reviews/display-reviews.component';
-import { TheatresComponent } from './theatres/theatres.component';
-import { DataService } from './services/data.service';
-import { TheatresNearbyService } from './services/theatres-nearby.service';
+import { CreateReviewComponent } from '../components/create-review/create-review.component';
+import { RatingComponent } from '../components/rating/rating.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { DisplayReviewsComponent } from '../components/display-reviews/display-reviews.component';
+import { TheatresComponent } from '../theatres/theatres.component';
+import { DataService } from './data.service';
+import { TheatresNearbyService } from './theatres-nearby.service';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { AppErrorHandler } from './common/validators/app-error-handler';
-import { SignupFormComponent } from './components/signup-form/signup-form.component';
-import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { NewReleasesComponent } from './components/new-releases/new-releases.component';
+import { AppErrorHandler } from '../common/validators/app-error-handler';
+import { SignupFormComponent } from '../components/signup-form/signup-form.component';
+import { ChangePasswordComponent } from '../components/change-password/change-password.component';
+import { NewReleasesComponent } from '../components/new-releases/new-releases.component';
 
 
 @NgModule({
